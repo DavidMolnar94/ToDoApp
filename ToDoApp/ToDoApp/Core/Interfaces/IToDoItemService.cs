ab71dbc6910cb16efb88b9b3f8033f431c5fa56c
Molni <davidmolnar94@gmail.com>
2021-12-12 13:26:24 +0100

    add ToDoitem model and service Interce definition

---
+++ ToDoApp/ToDoApp/Core/Interfaces/IToDoItemService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Core.Models;
+
+namespace ToDoApp.Core.Interfaces
+{
+    interface IToDoItemService
+    {
+        IEnumerable<ToDoItem> GetToDoItems();
+        ToDoItem GetById(int id);
+        ToDoItem Add(ToDoItem toDoItem);
+        ToDoItem Update(int id, ToDoItem toDoItem);
+        void Delete(int id);
+    }
+}
---
+++ ToDoApp/ToDoApp/Core/Services/TmpToDoItemService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Core.Interfaces;
+using ToDoApp.Core.Models;
+
+namespace ToDoApp.Core.Services
+{
+    public class TmpToDoItemService : IToDoItemService
+    {
+        public ToDoItem Add(ToDoItem toDoItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ToDoItem GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ToDoItem> GetToDoItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ToDoItem Update(int id, ToDoItem toDoItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
